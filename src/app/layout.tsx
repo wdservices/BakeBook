d38f86de86f1a0da6fdf7a398e1543ea35a48509
GOSPEL ONONWI <Spellz49@gmail.com>
2025-07-01 14:58:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" suppressHydrationWarning>
       <head />
       <body className={cn(
         "font-body antialiased flex flex-col min-h-screen",
